e5c8ecd761dca12a2c14dcfbe690ae79ec63ba7e
MounirG91 <mounir_guizani@hotmail.fr>
2016-01-07 09:52:53 +0100

    Gestion D'utilisateurs

---
+++ src/app/server/models/user.js
@@ -0,0 +1,32 @@
+/**
+ * User
+ *
+ * @module      :: Model
+ * @description :: Represent data model for the Users
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var User = new Schema({
+
+  username:    {
+    type    : String,
+    require : true
+  },
+  fullname:    {
+    type    : String,
+    require : true
+  },
+  mail:     {
+    type    : String,
+    require : true
+  },
+  passwd:   {
+    type: String,
+	require : true
+  }
+});
+
+module.exports = mongoose.model('User', User);
