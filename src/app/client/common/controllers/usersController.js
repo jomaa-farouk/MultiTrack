4c639dbf0f2843efbfd2795ab24fe2331fe34c04
farouk <jomaa.farouk@gmail.com>
2016-02-10 19:32:34 +0100

    update farouk

--- src/app/client/common/controllers/usersController.js
+++ src/app/client/common/controllers/usersController.js
@@ -67,18 +67,84 @@
 			console.log('password hash is ',$scope.user.passwd);
 
 			
-			var response = $http.post('/users', $scope.user);
-			response.error(function(data, status, header, config){
+/*			var response = $http.post('/users/', $scope.user);
+*/			/*response.error(function(data, status, header, config){
 				$scope.showError = true;
 				$scope.message = "Error : "+status+ " Existing username '"+$scope.user.username+"' or empty field(s)";
+				console.log ('user exist');
+			});*/
+
+                var input = document.getElementById('first_name');
+                var validityState_object = input.validity;
+
+ 				var input2 = document.getElementById('phone');
+                var validityState_object2 = input2.validity;
+
+ 				var inputmail = document.getElementById('email');
+ 				var mail = inputmail.value;
+                var mail_valid = true;
+
+ 				if (mail!=null && mail !='') {
+ 					
+ 					if (validateEmail (mail)) input.setCustomValidity('');
+ 					else 
+ 					{
+ 				     input.setCustomValidity('you must enter a valid mail'); 
+ 				     mail_valid = false;
+ 					 }
+
+                }
+
+                if(validityState_object.valueMissing)
+                    input.setCustomValidity('You must enter a username');
+                else
+                    input.setCustomValidity('');
+
+ 				if(validityState_object2.valueMissing)
+                    input2.setCustomValidity('You must enter a password');
+                else
+                    input2.setCustomValidity('');
+
+
+if (mail_valid && !validityState_object.valueMissing && !validityState_object2.valueMissing)
+            $http.post('/users' , $scope.user ).then(function (response) {
+          console.log ('we are here');
+          if (response.data.error != null) {console.log ('error');
+          		$scope.showError = true;
+				$scope.message = "Error : "+status+ " Existing username '"+$scope.user.username+"' or empty field(s)";
+				}
+			else  
+			{alert ('user created successfully');
+		    $location.path("/home");}
+	
+          return response.data;
 			});
-			response.success(function(data, status, header, config){
+
+/*if c pas bon
+          		$scope.showError = true;
+				$scope.message = "Error : "+status+ " Existing username '"+$scope.user.username+"' or empty field(s)";
+*/
+            /*success(function(data, status, header, config){
+				console.log($scope.user + " created successfly!");
+				$location.path("/home");
+			}).error(function(data, status, header, config){
+				console.log ('user exist');
+			});
+*/
+/*			response.success(function(data, status, header, config){
 				console.log($scope.user + " created successfly!");
 				$location.path("/home");
 			});
-
+*/
 			
 		};
+
+
+	function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+	}
+	
 	}]);
 })();
 
