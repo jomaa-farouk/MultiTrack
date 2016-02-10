7661f6fec9369792192f49da44fc6fdcdb748bcb
farouk <jomaa.farouk@gmail.com>
2016-02-10 20:52:05 +0100

    la magie noire

--- src/app/client/common/controllers/usersController.js
+++ src/app/client/common/controllers/usersController.js
@@ -159,36 +159,4 @@ console.log( validityState_object.valueMissing);
 })();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-//https://docs.angularjs.org/api/ngResource/service/$resource re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-	}
-	
-	}]);
-})();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //https://docs.angularjs.org/api/ngResource/service/$resource
