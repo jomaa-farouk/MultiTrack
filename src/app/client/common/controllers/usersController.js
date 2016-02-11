39adfa3398773d3c576c38defb255629703563e6
farouk <jomaa.farouk@gmail.com>
2016-02-11 19:34:19 +0100

    last commit, finallyy

--- src/app/client/common/controllers/usersController.js
+++ src/app/client/common/controllers/usersController.js
@@ -6,63 +6,63 @@
 
 	app.controller('UsersController', ['$scope', 'AuthFactory','UserFactory', 'UsersFactory', '$location', 
 		function($scope, AuthFactory, UserFactory, UsersFactory, $location){
-		var app = this;
+			var app = this;
 
-		
+			
 
-		if(!AuthFactory.check()){
-			$location.path('/login');
-		};
+			if(!AuthFactory.check()){
+				$location.path('/login');
+			};
 
 
-		$scope.logout = function(){
-			AuthFactory.logout();
-			$location.path('/home');
-		};
+			$scope.logout = function(){
+				AuthFactory.logout();
+				$location.path('/home');
+			};
 
-		$scope.editUser = function(uid){
-			$location.path('/user-detail/'+uid);
-		};
+			$scope.editUser = function(uid){
+				$location.path('/user-detail/'+uid);
+			};
 
-		$scope.deleteUser = function(uid){
-			UserFactory.delete({id:uid});
+			$scope.deleteUser = function(uid){
+				UserFactory.delete({id:uid});
+				$scope.users = UsersFactory.query();
+			};
+
+			$scope.createNewUser = function(){
+				$location.path('/user-creation');
+			}
+
 			$scope.users = UsersFactory.query();
-		};
 
-		$scope.createNewUser = function(){
-			$location.path('/user-creation');
-		}
-
-		$scope.users = UsersFactory.query();
-
-	}]);
+		}]);
 
 
 	app.controller('UserController', ['$scope', '$routeParams', 'UserFactory', '$location', 
 		function($scope, $routeParams, UserFactory, $location){
-		
-		$scope.updateUser = function(){
-			var $id = $routeParams.id;
-			UserFactory.update({id: $id}, $scope.user);
-			$location.path('/user-list');
-		};
+			
+			$scope.updateUser = function(){
+				var $id = $routeParams.id;
+				UserFactory.update({id: $id}, $scope.user);
+				$location.path('/user-list');
+			};
 
-		$scope.cancel = function(){
-			$location.path('/user-list');
-		};
+			$scope.cancel = function(){
+				$location.path('/user-list');
+			};
 
-		$scope.user = UserFactory.show({id: $routeParams.id});
+			$scope.user = UserFactory.show({id: $routeParams.id});
 
-	}]);
+		}]);
 
 	app.controller('UserCreation', ['$scope', '$http', 'UsersFactory', 'AuthFactory','$location', 
 		function($scope, $http, UsersFactory, AuthFactory, $location){
 
-		$scope.showError = false;
+			$scope.showError = false;
 			
-		$scope.createNewUser = function(){
-			var hash = AuthFactory.crypt($scope.user.passwd);
-			$scope.user.role = "User";
+			$scope.createNewUser = function(){
+				var hash = AuthFactory.crypt($scope.user.passwd);
+				$scope.user.role = "User";
 			//console.log('password hash is ',$scope.user.passwd);
 
 			
@@ -73,69 +73,69 @@
 				console.log ('user exist');
 			});*/
 
-                var input = document.getElementById('first_name');
-                var validityState_object = input.validity;
+var input = document.getElementById('first_name');
+var validityState_object = input.validity;
 
- 				var input2 = document.getElementById('phone');
-                var validityState_object2 = input2.validity;
+var input2 = document.getElementById('phone');
+var validityState_object2 = input2.validity;
 
- 				var inputmail = document.getElementById('email');
-                var validityState_object3 = inputmail.validity;
- 				var mail = inputmail.value;
-                var mail_valid = true;
+var inputmail = document.getElementById('email');
+var validityState_object3 = inputmail.validity;
+var mail = inputmail.value;
+var mail_valid = true;
 
-                if(validityState_object.valueMissing)
-                    input.setCustomValidity('You must enter a username');
-                else
-                    input.setCustomValidity('');
+if(validityState_object.valueMissing)
+	input.setCustomValidity('You must enter a username');
+else
+	input.setCustomValidity('');
 
- 				if(validityState_object2.valueMissing)
-                    input2.setCustomValidity('You must enter a password');
-                else
-                    input2.setCustomValidity('');
+if(validityState_object2.valueMissing)
+	input2.setCustomValidity('You must enter a password');
+else
+	input2.setCustomValidity('');
 
-				if (!validityState_object3.valueMissing) {
- 					
- 					if (validateEmail (mail)) {
- 						mail_valid = true;
- 						inputmail.setCustomValidity('');
- 					}
- 					else 
- 					{
- 				     inputmail.setCustomValidity('you must enter a valid E-mail'); 
- 				     mail_valid = false;
- 					 }
+if (!validityState_object3.valueMissing) {
+	
+	if (validateEmail (mail)) {
+		mail_valid = true;
+		inputmail.setCustomValidity('');
+	}
+	else 
+	{
+		inputmail.setCustomValidity('you must enter a valid E-mail'); 
+		mail_valid = false;
+	}
 
-                }
-        	    else inputmail.setCustomValidity('you must enter an E-mail'); 
+}
+else inputmail.setCustomValidity('you must enter an E-mail'); 
 
 
 
-		if (mail_valid && !validityState_object.valueMissing && !validityState_object2.valueMissing) {
-			$scope.user.passwd = hash;
-            $http.post('/users' , $scope.user ).then(function (response) {
-           if (response.data.error != null) {
-          		$scope.showError = true;
-				$scope.message = "Error : Existing username '"+$scope.user.username+"'";
-				}
-			else  
-			{
+if (mail_valid && !validityState_object.valueMissing && !validityState_object2.valueMissing) {
+	$scope.user.passwd = hash;
+	$http.post('/users' , $scope.user ).then(function (response) {
+		if (response.data.error != null) {
+			$scope.showError = true;
+			$scope.message = "Error : Existing username '"+$scope.user.username+"'";
+		}
+		else  
+		{
 			$scope.showError = false;
 			alert ('user created successfully');
-		    $location.path("/home");
-		    }
-			return response.data;
-			});
-			}		
-		};
+			$location.path("/home");
+		}
+		return response.data;
+	});
+}		
+};
 
 
-	function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-	}
-	
-	}]);
+function validateEmail(email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
+}
+
+}]);
 })();
 
 
--- src/app/client/lib/js/track-creation.js
+++ src/app/client/lib/js/track-creation.js
@@ -2,32 +2,32 @@
     var fileDrag = document.querySelector('#filedrag');
     var memFiles = new Array();
     var files = [];
-      
+    
     //on glisse sur la zone à déposer
-      function dragOverHandler(event) {
+    function dragOverHandler(event) {
       //alert("Drag over a droppable zone");
-    event.stopPropagation();
-    event.preventDefault();
+      event.stopPropagation();
+      event.preventDefault();
     }
 
     //on lache le fichier que l'on glissait
     function dragLeaveHandler(event) {
       //alert("drag leave");
-    event.target.classList.remove('draggedOver');
-      }
+      event.target.classList.remove('draggedOver');
+    }
     
       //On depose le fichier dans la zone correspondante
       function dropHandler(event) {
       //alert('drop event');
-    event.stopPropagation();
-    event.preventDefault();
-    event.target.classList.remove('draggedOver');
-    files = event.dataTransfer.files;
-    var filesLen = files.length;
-    var filenames = "";
+      event.stopPropagation();
+      event.preventDefault();
+      event.target.classList.remove('draggedOver');
+      files = event.dataTransfer.files;
+      var filesLen = files.length;
+      var filenames = "";
     //alert(filesLen);
     for(var i = 0 ; i < filesLen ; i++) {
-        filenames += '\n' + files[i].name;
+      filenames += '\n' + files[i].name;
       var li = document.createElement('li');
       li.textContent = files[i].name + " ";
       var progressBar = document.createElement('progress');
@@ -41,64 +41,64 @@
   }
 
 
-function verifySubmit () {
+  function verifySubmit () {
 
-                var input1 = document.getElementById('title');
-                var validityState_object = input1.validity;
-               
-                var input2 = document.getElementById('group');
-                var validityState_object2 = input2.validity;
-                
-                if (files.length == 0) 
-                 {
-                  $('#sp_upload').html('You must drop at least one file');
-                }
-                else {
-                  $('#sp_upload').html('');
-                }
-  
-  if (!validityState_object.valueMissing && !validityState_object2.valueMissing && !(files.length == 0) )   
-  saveForm ();           
-  
-  return false;    
-  
-  }
-
-    function saveForm() {
-
-      var pistes = [];
-      var s = "";
-      for(var i=0; i < files.length; i++)
-       { 
-        pistes.push (
-         {'pisteMp3' : files[i].name}
-          );
-      }
-       
-var pistesJson = JSON.stringify(pistes) ;
-
-var data = $('#myForm').serializeArray();
-
-data.push({name: 'piste', value: pistesJson});
-
-     $.ajax({
-                url:'/tracks',
-                type:'post',
-                data:data,
-                success:function(response, status, xhr){
-                    if (response.error==null) 
-                    sendFiles (true);    
-                    else 
-                    sendFiles (false);    
-
-                },
-                error:function ( ){
-                  alert ('Track upload fail');
-                }
-            }); 
-  }
+    var input1 = document.getElementById('title');
+    var validityState_object = input1.validity;
     
-    function sendFiles (success) {
+    var input2 = document.getElementById('group');
+    var validityState_object2 = input2.validity;
+    
+    if (files.length == 0) 
+    {
+      $('#sp_upload').html('You must drop at least one file');
+    }
+    else {
+      $('#sp_upload').html('');
+    }
+    
+    if (!validityState_object.valueMissing && !validityState_object2.valueMissing && !(files.length == 0) )   
+      saveForm ();           
+    
+    return false;    
+    
+  }
+
+  function saveForm() {
+
+    var pistes = [];
+    var s = "";
+    for(var i=0; i < files.length; i++)
+    { 
+      pistes.push (
+       {'pisteMp3' : files[i].name}
+       );
+    }
+    
+    var pistesJson = JSON.stringify(pistes) ;
+
+    var data = $('#myForm').serializeArray();
+
+    data.push({name: 'piste', value: pistesJson});
+
+    $.ajax({
+      url:'/tracks',
+      type:'post',
+      data:data,
+      success:function(response, status, xhr){
+        if (response.error==null) 
+          sendFiles (true);    
+        else 
+          sendFiles (false);    
+
+      },
+      error:function ( ){
+        alert ('Track upload fail');
+      }
+    }); 
+  }
+  
+  function sendFiles (success) {
    
    if (success) {
     var xhr2 = new XMLHttpRequest();
@@ -106,30 +106,29 @@ data.push({name: 'piste', value: pistesJson});
     xhr2.open('GET', '/api/folder/'+folder);
     xhr2.send();
 
-   
-        var xhr = new XMLHttpRequest();
+    
+    var xhr = new XMLHttpRequest();
 
-        xhr.open('POST', '/api/file');
-        var form = new FormData();
-        for(var i=0; i < files.length; i++)
-       { 
-        form.append('file',files[i]);
-        console.log (files.length);
-       }
-        xhr.send(form);
-
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-    alert("Track uploaded successfully");
-    window.location.replace("/");
+    xhr.open('POST', '/api/file');
+    var form = new FormData();            
+    for(var i=0; i < files.length; i++)
+    { 
+      form.append('file',files[i]);
+      console.log (files.length);
     }
-}}
+    xhr.send(form);
 
-   else
-      { alert("Track title exists already");
-        $('#droppedFiles').html ('');
-        files = []; 
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == XMLHttpRequest.DONE) {
+        alert("Track uploaded successfully");
+        window.location.replace("/");
       }
-     
-     }
+    }}
+
+    else
+      { alert("Track title exists already");
+    $('#droppedFiles').html ('');
+    files = []; 
+  }
   
+}
